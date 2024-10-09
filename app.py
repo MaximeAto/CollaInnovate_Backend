c4ae8a73f749eb3e1f35c7d7ee87096ac6f78d51
MaximeAto <maxime.nkeng@institutsaintjean.org>
2024-10-09 17:27:29 +0100

    maj

--- app.py
+++ app.py
@@ -3,10 +3,9 @@ from flask import Blueprint, jsonify, make_response, request, current_app
 from flask_jwt_extended import jwt_required, get_jwt_identity
 
 
+app = create_app()
 
 if __name__ == '__main__':
-  app = create_app()
-
   @app.route('/', methods=['GET'])
   def root():
       return jsonify({"message": "good job"})
